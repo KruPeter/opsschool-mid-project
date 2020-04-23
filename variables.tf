0ef9e05664a5c6a9b8af513eb7cac64d22578eb2
KruPeter <53606842+KruPeter@users.noreply.github.com>
2020-04-24 00:07:11 +0300

    Add files via upload

--- variables.tf
+++ variables.tf
@@ -7,6 +7,16 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
+variable vpc_id {
+  description = "AWS VPC id"
+  default     = "vpc-04d684c73d66559bf"
+}
+
+variable "ip" {
+  default = "109.66.225.222/32"
+  description = "my private ip"
+}
+
 variable "consul_version" {
   description = "The version of Consul to install (server and client)."
   default     = "1.4.0"
